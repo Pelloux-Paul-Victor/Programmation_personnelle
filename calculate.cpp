7934246a73946a10a533d40b1b712065eabe8600
Paul-Victor <pv.pelloux@gmail.com>
2026-02-01 11:30:49 +0100

    Add files via upload

---
+++ calculate.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int choix = 0;
+    int nb_nombre = 0;
+    int somme = 0;
+    int total = 0;
+    int total2;
+
+    cout << "Choisir type calcule : \n";
+    cout << "   1 - somme\n";
+    cout << "   2 - soustraction\n";
+    cout << "   3 - multiplication\n";
+    cout << "Votre choix : ";
+    cin >> choix;
+    if (choix == 3){
+        cout << "Calcul restreint pour 2 chiffres !\n";
+        nb_nombre = 2;
+    } else {
+        cout << "Combient de nombre a saisir ? ";
+        cin >> nb_nombre;
+    }
+
+
+    if (choix != 0){
+        while (nb_nombre != 0){
+            cout << "Saisir un chiffre : ";
+            cin >> total;
+            if (choix == 1){
+                somme += total;
+            } else if (choix == 2){
+                somme -= total;
+            } else if (choix == 3){
+                cout << "Saisir un chiffre : ";
+                cin >> total2;
+                somme = total*total2;
+                break;
+            }
+            nb_nombre--;
+        }
+    }else{
+        cout << "Erreur de saisie...";
+    }
+    cout << somme;
+    return somme;
+}
