3bdfbf5e7b4ed08c91db28aaa980df164bdd9cd5
Paul-Victor Pelloux <196071880+Pelloux-Paul-Victor@users.noreply.github.com>
2026-02-05 21:45:52 +0100

    Update recursivite.cpp

--- recursivite.cpp
+++ recursivite.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int fonction(int nb){
-
-    if (nb == 0){
+    if (nb==0){
         return 1;
     } else{
         return nb*fonction(nb-1);
     }
-
 }
-
 int main(){
-
     cout << fonction(4);
     return 0;
-
 }
