5f40fa6f30fb024e0498bb436a00ba384e358b47
Paul-Victor Pelloux <196071880+Pelloux-Paul-Victor@users.noreply.github.com>
2026-02-05 21:45:04 +0100

    Update exercice.cpp

--- exercice.cpp
+++ exercice.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-// fonction moyenne
 int moyenne(int* compteur, int* nombre){
     int moyenne=0;
-    int compteur2= *compteur;
-
+    int compteur2=*compteur;
     while (compteur2 !=0){
         compteur2--;
         cout << "saisir un nombre: ";
@@ -15,20 +12,15 @@ int moyenne(int* compteur, int* nombre){
     }
     return moyenne/ *compteur;
 }
-
-// fonction affiche
 void afficher(int resultat){
     cout << "Resultat de la moyenne: ";
     cout << resultat;
 }
-
 int main(){
     int compte;
     int nb=0;
     cout << "saisir un nombre de chiffre a saisir: ";
     cin >> compte;
-
     afficher(moyenne(&compte,&nb));
-
     return 0;
 }
