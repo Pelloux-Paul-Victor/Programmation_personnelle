a51eac9ee32d70a2807dcced1ef0627bf65c66c1
Paul-Victor Pelloux <196071880+Pelloux-Paul-Victor@users.noreply.github.com>
2026-02-05 21:46:36 +0100

    Update tableau.cpp

--- tableau.cpp
+++ tableau.cpp
@@ -1,30 +1,23 @@
 #include <stdio.h>
-
 const int ENTIER=10;
-
 int get_minimum(int tableau[ENTIER]){
     int minimum=tableau[0];
         for (int i=0; i<ENTIER; i++) {
             if (tableau[i]<minimum){
-                minimum = tableau[i];
+                minimum=tableau[i];
             }
     }
     return minimum;
 }
-
 int main() {
     int tableau[ENTIER];
     int entier;
     bool existe;
-
-    //Saisie des valeurs par l'utilisateur
     for (int i=0; i<ENTIER; i++) {
         printf("Saisir un entier pour l'indice %d: ", i);
         scanf("%d", &entier);
-        tableau[i] = entier;
+        tableau[i]=entier;
     }
-
-    //Affichage des valeurs que contient le tableau
     printf("Tableau=[");
     for (int i=0; i<ENTIER; i++) {
         if (i>0){
@@ -33,7 +26,6 @@ int main() {
         printf("%d", tableau[i]);
     }
     printf("]");
-
     printf("\n\nRecherche d'un entier dans le tableau...\n");
     printf("Saisir l'entier qu'il faut chercher: ");
     scanf("%d", &entier);
@@ -49,8 +41,6 @@ int main() {
     if (existe!=true){
         printf("L'entier n'existe pas\n");
     }
-
     printf("La valeur minimum du tableau est %d", get_minimum(tableau));
-
     return 0;
 }
