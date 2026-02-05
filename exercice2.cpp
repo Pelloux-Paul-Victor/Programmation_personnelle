4b3ddd507adcb37f692d7c448088c9bc6d60c46b
Paul-Victor Pelloux <196071880+Pelloux-Paul-Victor@users.noreply.github.com>
2026-02-05 21:45:28 +0100

    Update exercice2.cpp

--- exercice2.cpp
+++ exercice2.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 using namespace std;
-
-//calculer la moyenne de x entier saisi par l'utilisateur
-//on utilisera des pointeurs
 void calcul_moyenne(int *nb, int *moyenne){
     for (int i=0; i < *nb; i++){
         int nombre;
@@ -11,19 +8,12 @@ void calcul_moyenne(int *nb, int *moyenne){
         *moyenne += nombre;
     }
     *moyenne /= *nb;
-
 }
-
 int main(){
-
     int x, moyenne = 0;
     cout << "saisir ses nombres pour en faire une moyenne";
     cin >> x;
-
     calcul_moyenne(&x, &moyenne);
-
     cout << "moyenne = " << moyenne;
-
     return 0;
-
 }
