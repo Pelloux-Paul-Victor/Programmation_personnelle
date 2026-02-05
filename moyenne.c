05c4eab7b795c2476f093f53310d14c9470218c1
Paul-Victor Pelloux <196071880+Pelloux-Paul-Victor@users.noreply.github.com>
2026-02-05 12:37:16 +0100

    Create moyenne.c

---
+++ moyenne.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define BLUE(string) "\x1b[34m"string "\x1b[0m" 
+
+int main()
+{
+    int T[12]={18,19,20,21,22,19,20,85,21,20,19,18};
+    int moyenne=0;
+    for (int i=0; i<12; i++){
+        moyenne+=T[i];
+    }
+    moyenne /= 12;
+    for (int i=0; i<12; i++){
+        if ((T[i]<10.99) || (T[i]>61.33)){
+            T[i]=moyenne;
+        }
+    }
+    printf("T = ");
+    for (int i=0; i<12; i++){
+        if (i==7){
+            printf(""BLUE(" %d ")"",T[i]);
+        } else{
+            printf(" %d ", T[i]);
+        } 
+    }
+    return 0;
+}
