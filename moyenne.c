4b89999763b1f66ae549d2c6538129e9f23685d1
Paul-Victor Pelloux <196071880+Pelloux-Paul-Victor@users.noreply.github.com>
2026-02-05 21:36:16 +0100

    Update moyenne.c

--- moyenne.c
+++ moyenne.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #define BLUE(string) "\x1b[34m"string "\x1b[0m" 
 
-int main()
-{
+int main(){
     int T[12]={18,19,20,21,22,19,20,85,21,20,19,18};
     int moyenne=0;
-    for (int i=0; i<12; i++){
-        moyenne+=T[i];
-    }
+    int tmin=T[0];
+    int tmax=T[1];
+    for (int i=0; i<12; i++){moyenne+=T[i];}
     moyenne /= 12;
     for (int i=0; i<12; i++){
-        if ((T[i]<10.99) || (T[i]>61.33)){
-            T[i]=moyenne;
-        }
+        if ((T[i]<-10.99) || (T[i]>61.33)){T[i]=moyenne;}
+        if (T[i]<tmin){tmin=T[i];} if (T[i]>tmax){tmax=T[i];}
     }
-    printf("T = ");
+    printf("Tableau = [");
     for (int i=0; i<12; i++){
-        if (i==7){
-            printf(""BLUE(" %d ")"",T[i]);
-        } else{
-            printf(" %d ", T[i]);
-        } 
-    }
+        if (i==7){printf(BLUE(" %d"),T[i]);}
+        else{printf(" %d", T[i]);} 
+    } printf(" ]");
+    printf("\nMin = %d et Max = %d", tmin, tmax);
     return 0;
 }
